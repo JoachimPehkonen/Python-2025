b3d0fb9a87116c52d56b3c11d4422d310fef13c4
Joachim <JOP00060@laurea.fi>
2025-03-27 13:55:38 +0200

    tehtävät 1-3 tehty

--- Tuntitehtävät/Ehtolauseita, toistolauseita/Tehtävä 2.py
+++ Tuntitehtävät/Ehtolauseita, toistolauseita/Tehtävä 2.py
@@ -1,1 +1,31 @@
-luku1 = int(input())
+luku1 = int(input("Syötä ensimmäinen luku: "))
+luku2 = int(input("Syötä toinen luku: "))
+luku3 = int(input("Syötä kolmas luku: "))
+print(f"Syötä kolme lukua:{luku1} {luku2} {luku3}")
+
+if luku1 >= luku2 and luku1 >= luku3:
+    suurin = luku1
+    if luku2  >= luku3:
+        keski = luku2
+        pienin = luku3
+    else: 
+        keski = luku3
+        pienin = luku2
+elif luku2 >= luku1 and luku2 >= luku3:
+    suurin = luku2
+    if luku1 >= luku3:
+        keski = luku1
+        pienin = luku3
+    else:
+        keski = luku3
+        pienin = luku1
+else:
+    suurin = luku3
+    if luku1 >= luku2:
+        keski = luku1
+        pienin = luku2
+    else:
+        keski = luku2
+        pienin = luku1
+
+print(f"Järjestys: {pienin}, {keski}, {suurin}")
---
+++ Tuntitehtävät/Ehtolauseita, toistolauseita/Tehtävä 3.py
@@ -0,0 +1,35 @@
+ikä = int(input("Anna ikäsi: "))
+
+#Jos ikä on pienempi kuin 0 tai suurempi kuin 150, tulostetaan ”Virheellinen ikä!”
+if ikä == 0 or ikä == 150:
+    print("Virheellinen ikä!")
+
+#Jos ikä on alle 6 tulostetaan ”Esikoululainen”
+elif ikä < 6:
+    print("Esikoululainen")
+
+#Jos ikä on tasan 14 tulostetaan ”Haastava ikä”
+elif ikä == 14:
+    print("Haastava ikä")
+
+#Jos ikä on 16 - 18 tulostetaan ”Lähes aikuinen”
+elif ikä >= 16 and ikä <= 18:
+    print("Lähes aikuinen")
+
+#Jos ikä on suurempi kuin 18, mutta pienempi kuin 30 tulostetaan,”Olet täysi-ikäinen, muttet vielä keski-iän kriisissä”
+elif ikä > 18 and ikä < 30:
+    print("Olet täysi-ikäinen, muttet vielä keski-iän kriisissä")
+
+#Jos ikä on suurempi tai yhtä suuri kuin 30, mutta pienempi kuin 45 tulostetaan: ”Olet keski-iässä”
+elif ikä >= 30 and ikä < 45:
+    print("Olet keski-iässä")
+
+#Jos ikä on suurempi kuin 45 mutta alle 65 tulostetaan ”Vielä ehtii ennen eläkettä!”
+elif ikä >= 45 and ikä <= 65:
+    print("Vielä ehtii ennen eläkettä!")
+
+#Muuten ohjelma tulostaa ”Olet eläkeläinen”
+else:
+    print("Olet eläkeläinen")
+
+#Mitä ongelmia kyseisitä ehdoista löydät? Miten korjaisit ne?
